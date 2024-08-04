f1f059d9b490e813739ac86491a0d2ef16c738d7
Bobasso <mbobasso86@gmail.com>
2024-08-04 00:09:21 -0300

    Cambios 2

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ const listaGastos = [];
 let totalGastos = 0;
 const sumatoriaGastos = document.querySelector("#total-gastos");
 
-const botonSubmit = document.querySelector("#btn-submit"); // Boton que envia el formulario
-const agregarLocal = document.querySelector("#local"); // Local que ingresa en el input
-const agregarMonto = document.querySelector("#monto"); // Monto que ingresa en el input
+const botonSubmit = document.querySelector("#btn-submit");
+const agregarLocal = document.querySelector("#local");
+const agregarMonto = document.querySelector("#monto");
 
 const tablaGastos = document.querySelector("#tabla-gasto");
 
@@ -13,21 +13,28 @@ const crearLista = document.querySelector("#crear-lista");
 crearLista.addEventListener("submit", (e) => {
     e.preventDefault();
 
+    totalGastos += parseInt(agregarMonto.value); // Voy sumando el monto del gasto a un total
+
+    let gasto = {};
+    gasto.local = agregarLocal.value;
+    gasto.monto = parseInt(agregarMonto.value);
+
     const crearGasto = document.createElement("tr");
 
     const tdLocal = document.createElement("td");
-    tdLocal.innerText = agregarLocal.value;
+    tdLocal.innerText = gasto.local;
     crearGasto.append(tdLocal);
 
     const tdMonto = document.createElement("td");
-    tdMonto.innerText = "$" + agregarMonto.value;
-    totalGastos += parseInt(agregarMonto.value);
+    tdMonto.innerText = "$" + gasto.monto;
     crearGasto.append(tdMonto);
 
     tablaGastos.append(crearGasto);
+    listaGastos.push(gasto);
 
-    crearLista.reset();
+    // crearLista.reset();
     agregarLocal.focus();
 
+    console.log(listaGastos);
     sumatoriaGastos.innerText = `Usted gasto en total: $${totalGastos}`
 });
