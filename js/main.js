24c32daf36a8481080a895651d1ac90c7d8af7f8
Bobasso <mbobasso86@gmail.com>
2024-08-04 01:39:00 -0300

    Cambios 3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+// Creo lista vacia para sumar el objeto que sería gasto
 const listaGastos = [];
+
+// Creo un monto que empieza en 0 y después va sumando para devolver el total
 let totalGastos = 0;
 const sumatoriaGastos = document.querySelector("#total-gastos");
 
@@ -13,28 +16,42 @@ const crearLista = document.querySelector("#crear-lista");
 crearLista.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    totalGastos += parseInt(agregarMonto.value); // Voy sumando el monto del gasto a un total
-
     let gasto = {};
     gasto.local = agregarLocal.value;
     gasto.monto = parseInt(agregarMonto.value);
+    apareceLista(gasto);
 
-    const crearGasto = document.createElement("tr");
-
-    const tdLocal = document.createElement("td");
-    tdLocal.innerText = gasto.local;
-    crearGasto.append(tdLocal);
-
-    const tdMonto = document.createElement("td");
-    tdMonto.innerText = "$" + gasto.monto;
-    crearGasto.append(tdMonto);
-
-    tablaGastos.append(crearGasto);
-    listaGastos.push(gasto);
-
-    // crearLista.reset();
+    crearLista.reset();
     agregarLocal.focus();
 
+    totalGastos += parseInt(agregarMonto.value); //! Voy sumando el monto del gasto a un total
+    sumatoriaGastos.innerText = `Usted gasto en total: $${totalGastos}`;
     console.log(listaGastos);
-    sumatoriaGastos.innerText = `Usted gasto en total: $${totalGastos}`
-});
+})
+
+function apareceLista(objeto){
+    const listaLocal = listaGastos.map(item => item.local);
+    if(listaLocal.includes(objeto.local)){
+        const index = listaGastos.findIndex(item => item.local == objeto.local);
+        if (index !== -1) {
+            const numFinal = listaGastos[index].monto += objeto.monto;
+            const cambiarNum = document.querySelector(`#monto-${objeto.local}`);
+            cambiarNum.innerText = numFinal
+        }
+    }else{
+        listaGastos.push(objeto);
+
+        const crearGasto = document.createElement("tr");
+
+        const tdLocal = document.createElement("td");
+        tdLocal.innerText = objeto.local;
+        crearGasto.append(tdLocal);
+    
+        const tdMonto = document.createElement("td");
+        tdMonto.id = `monto-${objeto.local}`;
+        tdMonto.innerText = objeto.monto;
+        crearGasto.append(tdMonto);
+    
+        tablaGastos.append(crearGasto);
+    }
+}
