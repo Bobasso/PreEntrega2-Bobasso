19c874d472b9b67cf970503d6f907ae7ae069a29
Bobasso <mbobasso86@gmail.com>
2024-08-06 01:01:39 -0300

    Cambios 8

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
-// Creo lista vacia para sumar el objeto que sería gasto
-const listaGastos = [];
+const listaGastos = []; // Creo lista vacia para sumar el objeto que sería gasto
 
-// Creo un monto que empieza en 0 y después va sumando para devolver el total
-let totalGastos = 0;
+let totalGastos = 0; // Creo un monto que empieza en 0 y después va sumando para devolver el total
 const sumatoriaGastos = document.querySelector("#total-gastos");
 const mayorGasto = document.querySelector("#mayor-gasto");
 const menorGasto = document.querySelector("#menor-gasto");
 
+const tablaGastos = document.querySelector("#tabla-gasto"); // Id de toda la tabla
+const contenidoTabla = document.querySelector("#contenido-tabla") // Id donde va la info agregada por el usuario
+
+const crearLista = document.querySelector("#crear-lista"); // Id del formulario
 const botonSubmit = document.querySelector("#btn-submit");
-const agregarLocal = document.querySelector("#local");
-const agregarMonto = document.querySelector("#monto");
-
-const tablaGastos = document.querySelector("#tabla-gasto");
-
-const crearLista = document.querySelector("#crear-lista");
+const agregarLocal = document.querySelector("#local"); // Id input donde ingresa el local
+const agregarMonto = document.querySelector("#monto"); // Id input donde ingresa el monto gastado
 
 crearLista.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -68,7 +66,43 @@ function apareceLista(objeto){
         tdMonto.id = `monto-${objeto.local.toLowerCase()}`;
         tdMonto.innerText = `$${objeto.monto}`;
         crearGasto.append(tdMonto);
-    
-        tablaGastos.append(crearGasto);
+
+        const tdEliminar = document.createElement("td");
+        tdEliminar.className = "td-eliminar";
+        tdEliminar.innerText = "❌";
+        tdEliminar.addEventListener("click", ()=>{
+            crearGasto.remove();
+            const index = listaGastos.findIndex(item => item.local == objeto.local.toLowerCase());
+            if (index !== -1) {
+                totalGastos -= listaGastos[index].monto;
+                sumatoriaGastos.innerText = `Usted gasto en total: $${totalGastos}`;
+                listaGastos.splice(index, 1);
+
+                if(listaGastos.length > 0){
+                    const localMayorGasto = listaGastos.reduce((max, item) => (item.monto > max.monto ? item : max), listaGastos[0]);
+                    mayorGasto.innerText = `Local con el mayor gasto este mes: ` + localMayorGasto.local[0].toUpperCase() + localMayorGasto.local.substring(1);
+
+                    const localMenorGasto = listaGastos.reduce((min, item) => (item.monto < min.monto ? item : min), listaGastos[0]);
+                    menorGasto.innerText = `Local con el menor gasto este mes: ` + localMenorGasto.local[0].toUpperCase() + localMenorGasto.local.substring(1);
+                }else{
+                    mayorGasto.innerText = `Local con el mayor gasto este mes: `;
+                    menorGasto.innerText = `Local con el mayor gasto este mes: `;
+                }
+            }
+        });
+
+        crearGasto.append(tdEliminar);
+        contenidoTabla.append(crearGasto);
     }
 }
+
+const limpiarLista = document.querySelector("#btn-limpiar");
+limpiarLista.addEventListener("click", ()=>{
+    listaGastos.splice(0, listaGastos.length); // Vacio la lista para que pueda repetir nombres sin problema
+    tablaGastos.querySelector("tbody").innerHTML = ""; // Borro el contenido del html la tabla menos los títulos.
+    
+    totalGastos = 0; // Reset del total
+    sumatoriaGastos.innerText = ``; // 
+    mayorGasto.innerText = ``;      // -> Limpio los textos de abajo de la tabla
+    menorGasto.innerText = ``;      //
+});
